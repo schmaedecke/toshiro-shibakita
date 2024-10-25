fce784f052e4d96536ca72d85c110d69d68010cf
Renan Ferreira <79757077+meiotera@users.noreply.github.com>
2024-10-25 07:26:32 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
+    ProdutoID int,
     Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
+    Quantidade int,
+    Preco int,
+    Descricao varchar(50),
     Host varchar(50)
 );
